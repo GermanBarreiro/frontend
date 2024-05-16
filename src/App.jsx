c582862cd13e68535f3e853a449404d2ae266b4d
grm4n-21 <germanbarreiro2121@gmail.com>
2024-05-16 13:58:28 -0400

    ordenendo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import './index.css';
-import Login from './componet/loginUser.jsx';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './componet/loginUser';
+import Home from './componet/home';
+import Register from './componet/register';
 
 function App() {
   return (
-    <div >
+    <Router>
+      <Routes>
+        <Route path="*" element={<Navigate to="/home" />} />
       
-        <Login/>
-      
-    </div>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/componet/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-4xl text-white">Este es el home</h1>
+    </div>
+  );
+}
+
+export default Home;
